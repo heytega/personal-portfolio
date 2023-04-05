deb0150cc1e6ef389d43f636e80fcb8ef86adfee
heytega <akpovbiyoranthony@gmail.com>
2023-04-05 22:04:03 +0100

    creating the contact card

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import Style from './Contact.module.css';
+import logo from '../assets/Logo.svg';
+import img from '../assets/me.jpg';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section className={Style.cardContainer}>
+      <div className={Style.card}>
+        <div className={Style.header}>
+          <img className={Style.logo} src={logo} alt='logos' />
+          <h4>
+            Frontend <br /> Developer
+          </h4>
+        </div>
+        <div className={Style.imgContainer}>
+          <img className={Style.img} src={img} alt='my image' />
+        </div>
+        <h1 className={Style.address}>anthony@gmail.com</h1>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
