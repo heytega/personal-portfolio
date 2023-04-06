12102fb455d0a53621f2b8b280bcb010adf47e0e
heytega <akpovbiyoranthony@gmail.com>
2023-04-06 14:33:10 +0100

    continued building of contact card

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
       <div className={Style.card}>
         <div className={Style.header}>
           <img className={Style.logo} src={logo} alt='logos' />
-          <h4>
+          <h4 className={Style.title}>
             Frontend <br /> Developer
           </h4>
         </div>
@@ -17,6 +17,12 @@ const Contact = () => {
           <img className={Style.img} src={img} alt='my image' />
         </div>
         <h1 className={Style.address}>anthony@gmail.com</h1>
+        <h1 className={`${Style.address} ${Style.mb20}`}>
+          Base in Ogun State <br /> Nigeria
+        </h1>
+        <p className={Style.copyright}>
+          &copy; 2023 Fortae. All Rights Reserved
+        </p>
       </div>
     </section>
   );
