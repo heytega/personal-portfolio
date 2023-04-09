9ea3e04683fb97934c3dfbc5cb079db285a84586
heytega <akpovbiyoranthony@gmail.com>
2023-04-09 01:06:46 +0100

    clean up code to eradicate from use of svg icons

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import Style from './Socials.module.css';
-import github from '../assets/github-original.svg';
-import instagram from '../assets/instagram-original.svg';
-import linkedin from '../assets/linkedin-original.svg';
-import twitter from '../assets/twitter-fill.svg';
 import LineIcon from 'react-lineicons';
 
 const Socials = () => {
@@ -17,14 +13,11 @@ const Socials = () => {
       <li className={Style.handle}>
         <a href='#'>
           <LineIcon name='linkedin-original' style={{ color: '#565656' }} />
-          {/* <img className={Style.icon} src={linkedin} alt='linkedin' /> */}
         </a>
       </li>
       <li className={Style.handle}>
         <a href='#'>
           <LineIcon name='instagram' style={{ color: '#565656' }} />
-
-          {/* <img className={Style.icon} src={instagram} alt='instagram' /> */}
         </a>
       </li>
       <li className={Style.handle}>
