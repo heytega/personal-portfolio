3a04d3a5db264908cda178650723fa8e7e03e7d7
heytega <akpovbiyoranthony@gmail.com>
2023-04-09 01:32:44 +0100

    made hover effect on social icons

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,28 +1,59 @@
 import React from 'react';
 import Style from './Socials.module.css';
 import LineIcon from 'react-lineicons';
+import { useState } from 'react';
 
 const Socials = () => {
+  const [changeColor, setChangeColor] = useState(false);
   return (
     <ul className={Style.socials}>
-      <li className={Style.handle}>
-        <a href='#'>
-          <LineIcon name='twitter-original' style={{ color: '#565656' }} />
+      <li>
+        <a
+          href='#'
+          onMouseOver={() => setChangeColor(true)}
+          onMouseOut={() => setChangeColor(false)}
+          className={Style.handle}
+        >
+          <LineIcon
+            name='twitter-original'
+            style={{
+              color: changeColor ? '#0a58ca' : '#565656',
+              transition: '0.3s',
+            }}
+          />
         </a>
       </li>
-      <li className={Style.handle}>
-        <a href='#'>
-          <LineIcon name='linkedin-original' style={{ color: '#565656' }} />
+      <li>
+        <a href='#' className={Style.handle}>
+          <LineIcon
+            name='linkedin-original'
+            style={{
+              color: changeColor ? '#0a58ca' : '#565656',
+              transition: '0.3s',
+            }}
+          />
         </a>
       </li>
-      <li className={Style.handle}>
-        <a href='#'>
-          <LineIcon name='instagram' style={{ color: '#565656' }} />
+      <li>
+        <a href='#' className={Style.handle}>
+          <LineIcon
+            name='instagram'
+            style={{
+              color: changeColor ? '#0a58ca' : '#565656',
+              transition: '0.3s',
+            }}
+          />
         </a>
       </li>
-      <li className={Style.handle}>
-        <a href='#'>
-          <LineIcon name='github-original' style={{ color: '#565656' }} />
+      <li>
+        <a href='#' className={Style.handle}>
+          <LineIcon
+            name='github-original'
+            style={{
+              color: changeColor ? '#0a58ca' : '#565656',
+              transition: '0.3s',
+            }}
+          />
         </a>
       </li>
     </ul>
