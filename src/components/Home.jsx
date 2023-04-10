10ebb6d20f9dda3325e62ae86c63c01efee49f41
heytega <akpovbiyoranthony@gmail.com>
2023-04-10 22:52:21 +0100

    began styling of home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,9 @@ const Home = () => {
   return (
     <section className={Style.homeContainer}>
       <PageTag tag='introduce' icon='fa-house-user' />
+      <h1 className={Style.mainHeading}>
+        Hello I'm <span>Tega</span>, A Web Developer and Computer Scientist
+      </h1>
     </section>
   );
 };
