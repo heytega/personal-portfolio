1e33a36ccda1b00411dbecffe4eaccbd8c841586
heytega <akpovbiyoranthony@gmail.com>
2023-04-12 03:22:47 +0100

    import and set up lenis to control scroll behaviour

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
 import Style from './NavBar.module.css';
+import Lenis from '@studio-freight/lenis';
 
 const NavBar = () => {
+  const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)),
+    direction: 'vertical',
+    gestureDirection: 'vertical',
+    smooth: true,
+    smoothTouch: false,
+    touchMultiplier: 2,
+  });
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+
   return (
     <section className={Style.navBarContainer}>
       <ul className={Style.mainNavList}>
         <li>
-          <a href='#' className={Style.navItem}>
+          <a
+            href='#home'
+            className={Style.navItem}
+            onClick={(e) => lenis.scrollTo('#home')}
+          >
             <i class='fa-solid fa-house-user'></i>
           </a>
         </li>
         <li>
-          <a href='#' className={Style.navItem}>
+          <a
+            href='#about'
+            className={Style.navItem}
+            onClick={(e) => lenis.scrollTo('#about')}
+          >
             <i class='fa-solid fa-briefcase'></i>
           </a>
         </li>
