d049485715d71cfc3ecb43d393731eeb10e7bb65
heytega <akpovbiyoranthony@gmail.com>
2023-04-12 03:22:01 +0100

    implement scroll to home from navigation button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Style from './Home.module.css';
 import PageTag from './PageTag';
-import PortFolio from '../assets/round-text.png';
+import Projects from '../assets/round-text.png';
 
 const Home = () => {
   return (
-    <section className={Style.homeContainer}>
+    <section className={Style.homeContainer} id='home'>
       <PageTag tag='introduce' icon='fa-house-user' />
       <h1 className={Style.mainHeading}>
         Hello I'm <span className={Style.green}>Tega</span>, A Web Developer and
@@ -17,7 +17,20 @@ const Home = () => {
         bringing your ideas to life, one pixel at a time.
       </p>
       <a href='#' className={Style.portfolioLink}>
-        <img src={PortFolio} alt='link to see my project' />
+        <img
+          className={Style.circle}
+          src={Projects}
+          alt='link to see my projects'
+        />
+        <i
+          class='fa-solid fa-arrow-down-long fa-lg'
+          style={{
+            color: '#fff',
+            position: 'absolute',
+            fontSize: '40px',
+            display: 'block',
+          }}
+        ></i>
       </a>
     </section>
   );
