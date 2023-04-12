35cee164a39c1bd8c7d8c54a1d624266e1c8b041
heytega <akpovbiyoranthony@gmail.com>
2023-04-12 01:30:07 +0100

    completed and centeralised the side main nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,45 +4,43 @@ import Style from './NavBar.module.css';
 const NavBar = () => {
   return (
     <section className={Style.navBarContainer}>
-      <div className={Style.mainNav}>
-        <ul className={Style.mainNavList}>
-          <li>
-            <a href='#' className={Style.navItem}>
-              <i class='fa-solid fa-house-user'></i>
-            </a>
-          </li>
-          <li>
-            <a href='#' className={Style.navItem}>
-              <i class='fa-solid fa-briefcase'></i>
-            </a>
-          </li>
-          <li>
-            <a href='#' className={Style.navItem}>
-              <i class='fa-solid fa-bars-staggered'></i>
-            </a>
-          </li>
-          <li>
-            <a href='#' className={Style.navItem}>
-              <i class='fa-solid fa-kitchen-set'></i>
-            </a>
-          </li>
-          <li>
-            <a href='#' className={Style.navItem}>
-              <i class='fa-solid fa-rectangle-list'></i>
-            </a>
-          </li>
-          <li>
-            <a href='#' className={Style.navItem}>
-              <i class='fa-solid fa-shoe-prints'></i>
-            </a>
-          </li>
-          <li>
-            <a href='#' className={Style.navItem}>
-              <i class='fa-solid fa-pager'></i>
-            </a>
-          </li>
-        </ul>
-      </div>
+      <ul className={Style.mainNavList}>
+        <li>
+          <a href='#' className={Style.navItem}>
+            <i class='fa-solid fa-house-user'></i>
+          </a>
+        </li>
+        <li>
+          <a href='#' className={Style.navItem}>
+            <i class='fa-solid fa-briefcase'></i>
+          </a>
+        </li>
+        <li>
+          <a href='#' className={Style.navItem}>
+            <i class='fa-solid fa-bars-staggered'></i>
+          </a>
+        </li>
+        <li>
+          <a href='#' className={Style.navItem}>
+            <i class='fa-solid fa-kitchen-set'></i>
+          </a>
+        </li>
+        <li>
+          <a href='#' className={Style.navItem}>
+            <i class='fa-solid fa-rectangle-list'></i>
+          </a>
+        </li>
+        <li>
+          <a href='#' className={Style.navItem}>
+            <i class='fa-solid fa-shoe-prints'></i>
+          </a>
+        </li>
+        <li>
+          <a href='#' className={Style.navItem}>
+            <i class='fa-solid fa-pager'></i>
+          </a>
+        </li>
+      </ul>
     </section>
   );
 };
