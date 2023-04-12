af2a0afa735e91e28fc4318e0f8d911dca7e0ffb
heytega <akpovbiyoranthony@gmail.com>
2023-04-12 01:31:28 +0100

    continued implementation of home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Style from './Home.module.css';
 import PageTag from './PageTag';
+import PortFolio from '../assets/round-text.png';
 
 const Home = () => {
   return (
@@ -10,6 +11,14 @@ const Home = () => {
         Hello I'm <span className={Style.green}>Tega</span>, A Web Developer and
         Computer Scientist
       </h1>
+      <p className={Style.mainParagraph}>
+        My goal is to empower your brand with modern and intuitive interfaces,
+        <br />
+        bringing your ideas to life, one pixel at a time.
+      </p>
+      <a href='#' className={Style.portfolioLink}>
+        <img src={PortFolio} alt='link to see my project' />
+      </a>
     </section>
   );
 };
