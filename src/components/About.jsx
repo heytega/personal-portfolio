bf16b223fca02dca526b49b1ddee1364896a5daf
heytega <akpovbiyoranthony@gmail.com>
2023-04-14 15:50:40 +0100

    modified About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,17 @@ const About = () => {
   return (
     <section className={Style.aboutContainer} id='about'>
       <PageTag tag='about' icon='fa-user' />
+      <h1 className={Style.header}>
+        Every great development begin with an even{' '}
+        <span className={Style.green}>better story</span>
+      </h1>
+      <p className={Style.p}>
+        Since beginning my journey as a freelance designer nearly 8 years ago,
+        I've done remote work for agencies, consulted for startups, and
+        collaborated with talented people to create digital products for both
+        business and consumer use. I'm quietly confident, naturally curious, and
+        perpetually working on improving my chopsone design problem at a time.
+      </p>
     </section>
   );
 };
