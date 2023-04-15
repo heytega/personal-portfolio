8f2e6ca7bb608908b12068b7eeb89bd2f9b08cfa
heytega <akpovbiyoranthony@gmail.com>
2023-04-15 13:44:46 +0100

    modified the resume component

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -6,6 +6,30 @@ const Resume = () => {
   return (
     <section className={Style.resumeContainer} id='resume'>
       <PageTag tag='resume' icon='fa-briefcase' />
+      <h1 className={Style.header}>
+        Education & <span>Experience</span>
+      </h1>
+      <div className={Style.resumeTimeline}>
+        <p className={Style.date}>2020 - Present</p>
+        <h2 className={Style.jobTitle}>Framer Designer & Developer</h2>
+        <p className={Style.company}>Brunodee Agency</p>
+        <h2 className={Style.jobTitle}>Front-End WordPres Developer</h2>
+        <p className={Style.company}>Envato Market</p>
+      </div>
+      <div className={Style.resumeTimeline}>
+        <p className={Style.date}>2020 - Present</p>
+        <h2 className={Style.jobTitle}>Framer Designer & Developer</h2>
+        <p className={Style.company}>Brunodee Agency</p>
+        <h2 className={Style.jobTitle}>Front-End WordPres Developer</h2>
+        <p className={Style.company}>Envato Market</p>
+      </div>
+      <div className={Style.resumeTimeline}>
+        <p className={Style.date}>2020 - Present</p>
+        <h2 className={Style.jobTitle}>Framer Designer & Developer</h2>
+        <p className={Style.company}>Brunodee Agency</p>
+        <h2 className={Style.jobTitle}>Front-End WordPres Developer</h2>
+        <p className={Style.company}>Envato Market</p>
+      </div>
     </section>
   );
 };
