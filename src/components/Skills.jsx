a063eb7b8d12758579bc0cf7bb4c940a8199dc8e
heytega <akpovbiyoranthony@gmail.com>
2023-04-15 16:42:07 +0100

    set up the skill component for implementation

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,11 +4,18 @@ import Style from './Skills.module.css';
 
 const Skills = () => {
   return (
-    <section className='skillsContainer' id='skills'>
+    <section className={Style.skillsContainer} id='skills'>
       <PageTag tag='skills' icon='fa-kitchen-set' />
       <h1 className={Style.header}>
         My <span>Advantages</span>
       </h1>
+      <div className={Style.skills}>
+        <div className={Style.skill}>
+          <img className={Style.icon} src='' alt='figma' />
+          <h1 className={Style.percent}>93%</h1>
+        </div>
+        <p className={Style.name}>React</p>
+      </div>
     </section>
   );
 };
