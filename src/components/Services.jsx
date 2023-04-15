5bd3c9bd7800319f8084a694b90ef80aaa2a20c0
heytega <akpovbiyoranthony@gmail.com>
2023-04-15 14:08:42 +0100

    modified the resume component as well as the services component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PageTag from './PageTag';
+import Style from './Services.module.css';
 
 const Services = () => {
   return (
-    <section className='servicesContainer' id='services'>
+    <section className={Style.servicesContainer} id='services'>
       <PageTag tag='services' icon='fa-bars-staggered' />
     </section>
   );
