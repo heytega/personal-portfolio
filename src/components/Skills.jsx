d27ab097e670cb8ddb5b6da8758972071f604920
heytega <akpovbiyoranthony@gmail.com>
2023-04-15 16:28:35 +0100

    started implementation of skills components

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,6 +6,9 @@ const Skills = () => {
   return (
     <section className='skillsContainer' id='skills'>
       <PageTag tag='skills' icon='fa-kitchen-set' />
+      <h1 className={Style.header}>
+        My <span>Advantages</span>
+      </h1>
     </section>
   );
 };
