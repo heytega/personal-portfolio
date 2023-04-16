4d1b625a6506c774614a6cb179cbf44a6a581e1d
heytega <akpovbiyoranthony@gmail.com>
2023-04-16 22:21:58 +0100

    modified the skill component and add svg logo for material ui and react query

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PageTag from './PageTag';
 import Style from './Skills.module.css';
 import MUI from '../assets/material-ui.svg';
+import RQ from '../assets/react-query.svg';
 
 const Skills = () => {
   return (
@@ -10,6 +11,15 @@ const Skills = () => {
       <h1 className={Style.header}>
         My <span>Advantages</span>
       </h1>
+
+      <div className={Style.skills}>
+        <div className={Style.skill}>
+          <i class='fa-brands fa-square-js'></i>
+          <h1 className={Style.percent}>70%</h1>
+        </div>
+        <p className={Style.name}>Javascript</p>
+      </div>
+
       <div className={Style.skills}>
         <div className={Style.skill}>
           <i class='fa-brands fa-react fa-spin fa-spin-reverse'></i>
@@ -20,18 +30,18 @@ const Skills = () => {
 
       <div className={Style.skills}>
         <div className={Style.skill}>
-          <i class='fa-brands fa-react fa-spin fa-spin-reverse'></i>
-          <h1 className={Style.percent}>70%</h1>
-        </div>
-        <p className={Style.name}>Javascript</p>
-      </div>
-
-      <div className={Style.skills}>
-        <div className={Style.skill}>
           <img className={Style.icon} src={MUI} alt='mui' />
           <h1 className={Style.percent}>80%</h1>
         </div>
         <p className={Style.name}>Material UI</p>
+      </div>
+
+      <div className={Style.skills}>
+        <div className={Style.skill}>
+          <img className={Style.icon} src={RQ} alt='mui' />
+          <h1 className={Style.percent}>80%</h1>
+        </div>
+        <p className={Style.name}>Tanstack Query</p>
       </div>
     </section>
   );
