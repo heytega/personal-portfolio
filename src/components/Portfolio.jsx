82fa6d9c650685b9d1d3d3f72a51e9b24fd9f5b2
heytega <akpovbiyoranthony@gmail.com>
2023-04-17 00:08:04 +0100

    modified the portfolio section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import PageTag from './PageTag';
+import Style from './Portfolio.module.css';
 
 const Portfolio = () => {
   return (
-    <section className='portfolioContainer' id='portfolio'>
+    <section className={Style.portfolioContainer} id='portfolio'>
       <PageTag tag='portfolio' icon='fa-rectangle-list' />
+      <h1 className={Style.header}>
+        Featured <span>Projects</span>
+      </h1>
     </section>
   );
 };
