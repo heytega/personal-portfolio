0f2105d4e1c95751ccd83ea0d46475e31e551e95
heytega <akpovbiyoranthony@gmail.com>
2023-04-17 12:07:46 +0100

    modified the porfolio section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PageTag from './PageTag';
 import Style from './Portfolio.module.css';
+import Devops from '../assets/devops.png';
 
 const Portfolio = () => {
   return (
@@ -9,6 +10,14 @@ const Portfolio = () => {
       <h1 className={Style.header}>
         Featured <span>Projects</span>
       </h1>
+      <div className={Style.portfolioItems}>
+        <div className={Style.portfolioItem}>
+          <div className={Style.project}>
+            <img className={Style.projectImage} src={Devops} alt='' />
+          </div>
+          <h2 className={Style.projectDes}>Opspad (Devops Notepad)</h2>
+        </div>
+      </div>
     </section>
   );
 };
