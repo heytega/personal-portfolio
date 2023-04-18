1425fd5f561898db98d65acc1d9108213b28c06a
heytega <akpovbiyoranthony@gmail.com>
2023-04-18 13:56:26 +0100

    modified the form section

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,6 +6,9 @@ const Form = () => {
   return (
     <section className={Style.formContainer} id='form'>
       <PageTag tag='contact' icon='fa-pager' />
+      <h1 className={Style.header}>
+        Let's Work <span>Together!</span>
+      </h1>
     </section>
   );
 };
