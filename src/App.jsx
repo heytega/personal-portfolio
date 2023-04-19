452b2bb30e2103ea61d846f5657f43b2f9955c4e
heytega <akpovbiyoranthony@gmail.com>
2023-04-19 18:37:27 +0100

    modified app component

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Home from './components/Home';
 import Resume from './components/Resume';
-import Services from './components/Services';
+// import Services from './components/Services';
 import Skills from './components/Skills';
 import Portfolio from './components/Portfolio';
 import Form from './components/Form';
 import NavBar from './components/NavBar';
-import Experience from './components/Experience';
-import Testimonial from './components/Testimonial';
+// import Experience from './components/Experience';
+// import Testimonial from './components/Testimonial';
 import Lenis from '@studio-freight/lenis';
 
 const App = () => {
