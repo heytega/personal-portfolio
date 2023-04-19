bec191d69c0486a6b24025e051f0ff608f2ebcdb
heytega <akpovbiyoranthony@gmail.com>
2023-04-19 18:38:03 +0100

    modified button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import Style from './Button.module.css';
+import Lenis from '@studio-freight/lenis';
 
 const Button = () => {
+  // LENIS IMPLEMENTATION
+  const lenis = new Lenis();
+
+  lenis.on('scroll', (e) => {
+    // console.log(e);
+  });
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+
   return (
     <section className={Style.btnContainer}>
-      <a href='#' className={Style.contactBtn}>
+      <a
+        href='#form'
+        onClick={() => lenis.ScrollTo('#form')}
+        className={Style.contactBtn}
+      >
         <i class='fa-regular fa-envelope fa-lg'></i>
         <p className={Style.caption}>Hire me!</p>
       </a>
