8271c3ebd1ddbbc1e56a7e41da69a62716827b3f
heytega <akpovbiyoranthony@gmail.com>
2023-04-21 14:18:36 +0100

    modified the app component

--- src/App.jsx
+++ src/App.jsx
@@ -11,26 +11,8 @@ import Form from './components/Form';
 import NavBar from './components/NavBar';
 // import Experience from './components/Experience';
 // import Testimonial from './components/Testimonial';
-import Lenis from '@studio-freight/lenis';
 
 const App = () => {
-  // LENIS INTEGRATION
-  const lenis = new Lenis({
-    duration: 1.2,
-    easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)),
-    direction: 'vertical',
-    gestureDirection: 'vertical',
-    smoothWheel: true,
-    smoothTouch: false,
-    touchMultiplier: 2,
-  });
-
-  function raf(time) {
-    lenis.raf(time);
-    requestAnimationFrame(raf);
-  }
-
-  requestAnimationFrame(raf);
   return (
     <div className={style.mainContainer}>
       <div className={style.contactContainer}>
