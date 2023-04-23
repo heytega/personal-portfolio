d63722e3fb8a089af74116eae24f51cee0c65c35
heytega <akpovbiyoranthony@gmail.com>
2023-04-23 01:25:06 +0100

    set up test loader

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,38 @@ import Skills from './components/Skills';
 import Portfolio from './components/Portfolio';
 import Form from './components/Form';
 import NavBar from './components/NavBar';
+import video from './assets/video.mp4';
+import { useState } from 'react';
+import { useEffect } from 'react';
 // import Experience from './components/Experience';
 // import Testimonial from './components/Testimonial';
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const addLoader = () => setLoading(true);
+    const removeLoader = () => setLoading(false);
+    window.addEventListener('DOMContentLoaded', addLoader);
+    window.removeEventListener('DOMContentLoaded', addLoader);
+    window.addEventListener('load', removeLoader);
+    window.removeEventListener('load', removeLoader);
+  }, []);
+
+  if (loading) {
+    return <div className={style.loading}>Loading</div>;
+  }
   return (
     <div className={style.mainContainer}>
+      <video
+        className={style.video}
+        src={video}
+        controls
+        muted
+        autoPlay
+        loop
+        type='video/mp4'
+      />
       <div className={style.contactContainer}>
         <Contact />
       </div>
