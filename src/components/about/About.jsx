978aac8324977dcf099fd7f63bfbc539ea31dbc1
heytega <akpovbiyoranthony@gmail.com>
2023-04-27 16:11:13 +0100

    modified the about section content and heading

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,15 +7,28 @@ const About = () => {
     <section className={Style.aboutContainer} id='about'>
       <PageTag tag='about' icon='fa-user' />
       <h1>
-        Every great design begin with an even{' '}
-        <span className={Style.green}>better story</span>
+        {/* Every great design begin with an even{' '}
+        <span className={Style.green}>better story</span> */}
+        My Tech Odyssey: Navigating
+        <span> Inno</span>vation, <span>Crea</span>tivity, and
+        <span> Gro</span>wth
       </h1>
       <p className={Style.p}>
-        Since beginning my journey as a freelance designer nearly 8 years ago,
-        I've done remote work for agencies, consulted for startups, and
-        collaborated with talented people to create digital products for both
-        business and consumer use. I'm quietly confident, naturally curious, and
-        perpetually working on improving my chopsone design problem at a time.
+        My journey in tech began with a passion for developing web and desktop
+        applications that streamline workflows and meet the needs of users. As a
+        frontend developer, my focus has always been on delivering a seamless
+        user experience that incorporates innovative ideas and the latest
+        technologies. <br />
+        <br /> During my time as a Frontend ReactJS Developer Intern with
+        HNG/Zuri, I gained valuable experience building web and desktop
+        applications. This experience, coupled with my ability to work
+        collaboratively in an agile environment and my attention to detail, has
+        prepared me for new challenges in the tech industry. <br />
+        <br /> I am excited to take on new opportunities and work with dynamic
+        and fast-growing companies. I am committed to delivering pixel-perfect
+        design implementation and user-friendly experiences, and I believe my
+        passion for innovation and my fast learning ability make me an excellent
+        candidate for any tech role.
       </p>
     </section>
   );
--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -10,25 +10,23 @@ const Resume = () => {
         Education & <span>Experience</span>
       </h1>
       <div className={Style.resumeTimeline}>
-        <p className={Style.date}>2020 - Present</p>
-        <h2 className={Style.jobTitle}>Framer Designer & Developer</h2>
-        <p className={Style.company}>Brunodee Agency</p>
-        <h2 className={Style.jobTitle}>Front-End WordPres Developer</h2>
-        <p className={Style.company}>Envato Market</p>
+        <p className={Style.date}> DEC 2022 - Present</p>
+
+        <h2 className={Style.jobTitle}>Front-End Web Developer</h2>
+        <p className={Style.company}>Opspad</p>
       </div>
       <div className={Style.resumeTimeline}>
-        <p className={Style.date}>2020 - Present</p>
-        <h2 className={Style.jobTitle}>Framer Designer & Developer</h2>
-        <p className={Style.company}>Brunodee Agency</p>
-        <h2 className={Style.jobTitle}>Front-End WordPres Developer</h2>
-        <p className={Style.company}>Envato Market</p>
+        <p className={Style.date}>OCT - DEC 2022</p>
+        <h2 className={Style.jobTitle}>Front-End Web Developer Intern</h2>
       </div>
       <div className={Style.resumeTimeline}>
-        <p className={Style.date}>2020 - Present</p>
-        <h2 className={Style.jobTitle}>Framer Designer & Developer</h2>
-        <p className={Style.company}>Brunodee Agency</p>
-        <h2 className={Style.jobTitle}>Front-End WordPres Developer</h2>
-        <p className={Style.company}>Envato Market</p>
+        <p className={Style.date}>SEP 2018 - MAY 2021</p>
+        <h2 className={Style.jobTitle}>
+          Higher National Diploma in Computer Science
+        </h2>
+        <p className={Style.company}>Federal Polytechnic Auchi</p>
+        <h2 className={Style.jobTitle}>National Diploma in Computer Science</h2>
+        <p className={Style.company}>Federal Polytechnic Auchi</p>
       </div>
     </section>
   );
