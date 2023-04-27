eef652785ab49295bdab27445045962288e1767a
heytega <akpovbiyoranthony@gmail.com>
2023-04-27 16:25:13 +0100

    modified the paragraph on the about page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -18,14 +18,19 @@ const About = () => {
         applications that streamline workflows and meet the needs of users. As a
         frontend developer, my focus has always been on delivering a seamless
         user experience that incorporates innovative ideas and the latest
-        technologies. <br />
-        <br /> During my time as a Frontend ReactJS Developer Intern with
-        HNG/Zuri, I gained valuable experience building web and desktop
-        applications. This experience, coupled with my ability to work
-        collaboratively in an agile environment and my attention to detail, has
-        prepared me for new challenges in the tech industry. <br />
-        <br /> I am excited to take on new opportunities and work with dynamic
-        and fast-growing companies. I am committed to delivering pixel-perfect
+        technologies.
+      </p>
+      <p className={Style.p}>
+        {' '}
+        During my time as a Frontend ReactJS Developer Intern with HNG/Zuri, I
+        gained valuable experience building web and desktop applications. This
+        experience, coupled with my ability to work collaboratively in an agile
+        environment and my attention to detail, has prepared me for new
+        challenges in the tech industry.
+      </p>
+      <p className={Style.p}>
+        I am excited to take on new opportunities and work with dynamic and
+        fast-growing companies. I am committed to delivering pixel-perfect
         design implementation and user-friendly experiences, and I believe my
         passion for innovation and my fast learning ability make me an excellent
         candidate for any tech role.
