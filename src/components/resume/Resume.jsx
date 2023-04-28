0d6d68d7196b81252adddf174617da21fb2b871e
heytega <akpovbiyoranthony@gmail.com>
2023-04-28 13:53:29 +0100

    clean-up on the resume component

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PageTag from '../pageTag/PageTag';
 import Style from './Resume.module.css';
+import ResumeData from './ResumeData';
 
 const Resume = () => {
   return (
@@ -9,25 +10,17 @@ const Resume = () => {
       <h1>
         Education & <span>Experience</span>
       </h1>
-      <div className={Style.resumeTimeline}>
-        <p className={Style.date}> DEC 2022 - Present</p>
+      {ResumeData.map((tml) => (
+        <div className={Style.resumeTimeline}>
+          <p className={Style.date}>{tml.date}</p>
 
-        <h2 className={Style.jobTitle}>Front-End Web Developer</h2>
-        <p className={Style.company}>Opspad</p>
-      </div>
-      <div className={Style.resumeTimeline}>
-        <p className={Style.date}>OCT - DEC 2022</p>
-        <h2 className={Style.jobTitle}>Front-End Web Developer Intern</h2>
-      </div>
-      <div className={Style.resumeTimeline}>
-        <p className={Style.date}>SEP 2018 - MAY 2021</p>
-        <h2 className={Style.jobTitle}>
-          Higher National Diploma in Computer Science
-        </h2>
-        <p className={Style.company}>Federal Polytechnic Auchi</p>
-        <h2 className={Style.jobTitle}>National Diploma in Computer Science</h2>
-        <p className={Style.company}>Federal Polytechnic Auchi</p>
-      </div>
+          <h2 className={Style.jobTitle}>{tml.title1}</h2>
+          <p className={Style.company}>{tml.company1}</p>
+
+          <h2 className={Style.jobTitle}>{tml.title2}</h2>
+          <p className={Style.company}>{tml.company2}</p>
+        </div>
+      ))}
     </section>
   );
 };
---
+++ src/components/resume/ResumeData.js
@@ -0,0 +1,26 @@
+const ResumeData = [
+  {
+    date: 'DEC 2022 - Present',
+    title1: 'Front-End Web Developer',
+    company1: 'Opspad',
+    title2: '',
+    company2: '',
+  },
+  {
+    date: 'OCT - DEC 2022',
+    title1: 'Front-End Web Developer Intern',
+    company1: 'Zuri / HNG Internship',
+    title2: '',
+    company2: '',
+  },
+  {
+    date: 'SEP 2018 - MAY 2021',
+    title1:
+      'Front-End Web DeveloperHigher National Diploma in Computer Science',
+    company1: 'Federal Polytechnic Auchi',
+    title2: 'National Diploma in Computer Science',
+    company2: 'Federal Polytechnic Auchi',
+  },
+];
+
+export default ResumeData;
