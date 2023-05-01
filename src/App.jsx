1762334327f15bca3c9e44eb4b3905ac527bcaed
heytega <akpovbiyoranthony@gmail.com>
2023-05-01 13:36:54 +0100

    modified the loading functionality

--- src/App.jsx
+++ src/App.jsx
@@ -12,98 +12,91 @@ import NavBar from './components/nav/NavBar';
 import newVideo from './assets/video.mp4';
 import { useState } from 'react';
 import { useEffect } from 'react';
-import { LazyLoadComponent } from 'react-lazy-load-image-component';
 
 const App = () => {
   const [loading, setLoading] = useState(true);
   const [activeIndex, setActiveIndex] = useState(0);
 
-  // useEffect(() => {
-  //   window.addEventListener('load', () => {
-  //     setLoading(false);
-  //   });
+  useEffect(() => {
+    const images = document.getElementsByTagName('img');
+    const videos = document.getElementsByTagName('video');
+    const icons = document.getElementsByTagName('i');
+    const spans = document.getElementsByTagName('span');
+    const promises = [];
 
-  // const images = document.getElementsByTagName('img');
-  // const videos = document.getElementsByTagName('video');
-  // const icons = document.getElementsByTagName('i');
-  // const spans = document.getElementsByTagName('span');
-  // const promises = [];
+    // An array of promises for all images and videos
+    for (let i = 0; i < images.length; i++) {
+      promises.push(
+        new Promise((resolve) => {
+          images[i].onload = resolve;
+        })
+      );
+    }
 
-  // // An array of promises for all images and videos
-  // for (let i = 0; i < images.length; i++) {
-  //   promises.push(
-  //     new Promises((resolve) => {
-  //       images[i].onload = resolve;
-  //     })
-  //   );
-  // }
+    for (let i = 0; i < videos.length; i++) {
+      promises.push(
+        new Promise((resolve) => {
+          videos[i].onloadeddata = resolve;
+        })
+      );
+    }
 
-  // for (let i = 0; i < videos.length; i++) {
-  //   promises.push(
-  //     new Promise((resolve) => {
-  //       videos[i].onloadeddata = resolve;
-  //     })
-  //   );
-  // }
+    for (let i = 0; i < icons.length; i++) {
+      promises.push(
+        new Promise((resolve) => {
+          icons[i].onload = resolve;
+        })
+      );
+    }
 
-  // for (let i = 0; i < icons.length; i++) {
-  //   promises.push(
-  //     new Promise((resolve) => {
-  //       icons[i].onload = resolve;
-  //     })
-  //   );
-  // }
+    for (let i = 0; i < spans.length; i++) {
+      promises.push(
+        new Promise((resolve) => {
+          spans[i].onload = resolve;
+        })
+      );
+    }
 
-  // for (let i = 0; i < spans.length; i++) {
-  //   promises.push(
-  //     new Promise((resolve) => {
-  //       spans[i].onload = resolve;
-  //     })
-  //   );
-  // }
+    // wait for all promises to resolve before setting isLoading to false
+    Promise.all(promises).then(() => setLoading(false));
 
-  // // wait for all promises to resolve before setting isLoading to false
-  // Promise.all(promises).then(() => setLoading(false));
-
-  // // Cancel promises if component unmounts before they resolve
-  // return () => promises.forEach((promise) => promise.cancel());
-  // }, []);
+    // Cancel promises if component unmounts before they resolve
+    return () => promises.forEach((promise) => promise.cancel());
+  }, []);
 
   if (loading) {
     return <div className={style.loading}>Loading</div>;
   }
   return (
-    <LazyLoadComponent onContentVisible={() => setLoading(false)}>
-      <div className={style.mainContainer}>
-        <video
-          className={style.video}
-          controls={false}
-          src={newVideo}
-          muted
-          // autoStart
-          autoPlay
-          loop
-          type='video/mp4'
-          playsInline
-        />
+    <div className={style.mainContainer}>
+      <video
+        className={style.video}
+        controls={false}
+        src={newVideo}
+        muted
+        // autoStart
+        autoPlay
+        loop
+        type='video/mp4'
+        playsInline
+      />
 
-        <div className={style.contactContainer}>
-          <Contact />
-        </div>
-        <div className={style.timeline}>
-          <Home activeIndex={activeIndex} />
-          <About activeIndex={activeIndex} />
-          <Resume activeIndex={activeIndex} />
-          {/* <Services /> */}
-          <Skills activeIndex={activeIndex} />
-          <Portfolio activeIndex={activeIndex} />
-          {/* <Testimonial/> */}
-          {/* <Experience /> */}
-          <Form activeIndex={activeIndex} />
-        </div>
-        <NavBar setActiveIndex={setActiveIndex} />
+      <div className={style.contactContainer}>
+        <Contact />
       </div>
-    </LazyLoadComponent>
+      <div className={style.timeline}>
+        <Home activeIndex={activeIndex} />
+        <About activeIndex={activeIndex} />
+        <Resume activeIndex={activeIndex} />
+        {/* <Services /> */}
+        <Skills activeIndex={activeIndex} />
+        <Portfolio activeIndex={activeIndex} />
+        {/* <Testimonial/> */}
+        {/* <Experience /> */}
+        <Form activeIndex={activeIndex} />
+      </div>
+      <NavBar setActiveIndex={setActiveIndex} />
+    </div>
   );
 };
 
