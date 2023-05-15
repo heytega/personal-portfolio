974b1a7e23ccc0540b2434ab8897a63a6a05faa3
heytega <akpovbiyoranthony@gmail.com>
2023-05-16 00:54:25 +0100

    modified the Page Tag component

--- src/components/pageTag/PageTag.jsx
+++ src/components/pageTag/PageTag.jsx
@@ -3,40 +3,13 @@ import Style from './PageTag.module.css';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { useEffect } from 'react';
+import animatePageTag from '../../animations/animatePageTag';
 
 const PageTag = ({ icon, tag }) => {
   gsap.registerPlugin(ScrollTrigger);
 
   useEffect(() => {
-    let sections = window.document.querySelectorAll('.animateTag');
-    const Array = gsap.utils.toArray(sections);
-    console.log(Array);
-    console.log(sections);
-    Array.forEach((c) =>
-      gsap.fromTo(
-        c,
-        {
-          opacity: 0,
-          y: 40,
-        },
-        {
-          opacity: 1,
-          y: 0,
-          delay: 0.5,
-          // duration: 1.5,
-          duration: 2,
-          ease: 'power1.out',
-          scrollTrigger: {
-            trigger: c,
-            start: 'top 95%',
-            end: '+=50',
-            toggleActions: 'restart, none, reverse, pause',
-            scrub: 1,
-            // markers: true,
-          },
-        }
-      )
-    );
+    animatePageTag();
   });
 
   return (
