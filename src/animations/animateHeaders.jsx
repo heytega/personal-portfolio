a5119e5090dabfefc2eff95be18df97ba0b03861
heytega <akpovbiyoranthony@gmail.com>
2023-05-16 23:52:02 +0100

    modified the animations for the PageTag and the Header component

--- src/animations/animateHeaders.jsx
+++ src/animations/animateHeaders.jsx
@@ -5,8 +5,6 @@ gsap.registerPlugin(ScrollTrigger);
 const headerAnimation = () => {
   const sections = window.document.querySelectorAll('.animateHeader');
   const Array = gsap.utils.toArray(sections);
-  console.log(Array);
-  console.log(sections);
   Array.forEach((c) =>
     gsap.fromTo(
       c,
@@ -19,11 +17,11 @@ const headerAnimation = () => {
         y: 0,
         // delay: 1.5,
 
-        duration: 2.5,
+        // duration: 2.5,
         ease: 'power1.out',
         scrollTrigger: {
           trigger: c,
-          start: 'top 90%',
+          start: 'top 92%',
           end: '+=50',
           toggleActions: 'restart, none, reverse, pause',
           scrub: 1,
