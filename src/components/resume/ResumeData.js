315841a857d964ab22d73c865a32fc38709b12b6
heytega <akpovbiyoranthony@gmail.com>
2024-06-21 21:24:18 +0100

    I modified my work experience

--- src/components/resume/ResumeData.js
+++ src/components/resume/ResumeData.js
@@ -1,7 +1,17 @@
 const ResumeData = [
   {
+    date: 'APR 2024 - Present',
+    title1: 'Microsoft 365 Administrator',
+    company1: 'Iykkmann Multilinks Limited',
+  },
+  {
+    date: 'AUG 2023 - APR 2024',
+    title1: 'Technical Support Engineer Tier II',
+    company1: 'Tek Experts',
+  },
+  {
     date: 'DEC 2022 - Present',
-    title1: 'Front-End Web Developer',
+    title1: 'Front-End Web Developer / Microsoft 365 Administrator',
     company1: 'Opspad',
     title2: '',
     company2: '',
